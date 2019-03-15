a82e47f487e4cd7bb7ecc4ae0c50c8924b57109d
mckasap <mckasap@gmail.com>
2019-03-15 14:33:04 +0300

    egg timer

--- app/src/main/java/org/kasapbasi/week20190601/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week20190601/MainActivity.java
@@ -2,8 +2,15 @@ package org.kasapbasi.week20190601;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void ControlClick(View v){
+
+        Log.i("Test","clicked");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
